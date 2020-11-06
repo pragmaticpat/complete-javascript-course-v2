28639ac1311ac662c40209214be0dcf7e196dc77
pragmaticpat <pragmaticpat@me.com>
2020-11-06 12:00:09 -0500

    handling the escape key now

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -11,7 +11,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 btnCloseModal.addEventListener('click', closeModal);
+
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.code === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
 
 function openModal() {
   modal.classList.remove('hidden');
