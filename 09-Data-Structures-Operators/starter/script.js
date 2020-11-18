4537867a138a6cac766d3ccfbd42b143ded3d4b6
pragmaticpat <pragmaticpat@me.com>
2020-11-18 11:31:52 -0500

    completed code challenge - waiting to see what Jonas did...

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,7 +1,92 @@
 'use strict';
 
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3
+// total of minutes between, divided by number of events
+let total = 0;
+let prior = 0;
+for (const [key, val] of gameEvents) {
+  total += key - prior;
+  prior = key;
+}
+
+console.log(
+  `An event happened, on average, every ${total / gameEvents.size} minutes`
+);
+
+// 4
+for (const [time, event] of gameEvents) {
+  console.log(
+    `${time > 45 ? '[SECOND HALF] ' : '[FIRST HALF] '} ${time}: ${event}`
+  );
+}
+
 /** Maps */
 
+// Setting multiple vals into a map
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct! 🎉'],
+  [false, 'Try again! 💩'],
+]);
+
+console.log(question);
+
+// convert object to map via new Map(Object.entries(obj));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = 3; //Number(prompt('Your answer'));
+console.log(question.get(answer === question.get('correct')));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
 const rest = new Map();
 rest.set('name', 'classico italiano');
 rest.set(1, 'firenze, italy');
@@ -34,7 +119,7 @@ rest.set(arr, 'Test');
 rest.set(document.querySelector('h1'), 'heading');
 console.log(rest);
 console.log(rest.get(arr));
-
+*/
 /*
 // Sets - unique lists
 const orderSet = new Set(['pasta', 'pizza', 'pizza', 'pasta', 'pizza']);
@@ -81,7 +166,7 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 
 GOOD LUCK 😀
 */
-
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
