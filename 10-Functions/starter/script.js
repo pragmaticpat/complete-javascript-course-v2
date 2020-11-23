5cf8537105843525d714fad78b9c721bb882b8bf
pragmaticpat <pragmaticpat@me.com>
2020-11-23 10:19:39 -0500

    completed first code challenge in this section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
-
 /*
 const bookings = [];
+
 
 const createBooking = function (
   flightNum,
@@ -197,7 +197,7 @@ greetArrow('Hello')('Arrow function!');
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/* 
+/*
 Let's build a simple poll app!
 
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
@@ -212,10 +212,10 @@ Here are your tasks:
         2: Rust
         3: C++
         (Write option number)
-  
+
   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
 HINT: Use many of the tools you learned about in this and the last section 😉
@@ -233,4 +233,31 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const vote = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}(Write option number)`
+      )
+    );
+
+    typeof vote === 'number' &&
+      vote < this.answers.length &&
+      this.answers[vote]++; // Jonas's answer
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'string')
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    else if (type === 'array') console.log(this.answers);
+    else console.log("Valid options include 'string' and 'array'");
+  },
 };
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
