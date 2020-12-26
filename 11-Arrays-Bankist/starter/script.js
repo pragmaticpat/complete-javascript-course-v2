561da1236b9a07d4faf1517a7c94d393b419c283
Patrick Sullivan <pragmaticpat@gjsmba.local>
2020-12-26 12:36:55 -0500

    well that was easy! this is how I actually did the last challenge anyway...

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -351,3 +351,26 @@ const totalDeposits = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDeposits);
+
+/* 
+Coding Challenge #3
+
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanage = dogAges =>
+  dogAges
+    .map(age => (age > 2 ? 16 + age * 4 : age * 2))
+    .filter(age => age > 18)
+    .reduce((acc, age, _, arr) => acc + age / arr.length, 0); //ahh! I like this even better!
+
+console.log('Here is the first test data set');
+console.log(calcAverageHumanage([5, 2, 4, 1, 15, 8, 3]));
+
+console.log('Here is the second test data set');
+console.log(calcAverageHumanage([16, 6, 10, 5, 6, 1, 4]));
