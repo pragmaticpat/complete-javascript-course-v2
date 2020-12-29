c798f5d7692260b5a7816578c6c379896b401227
Patrick Sullivan <pragmaticpat@gjsmba.local>
2020-12-29 14:04:27 -0500

    getting up to date

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,39 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+
+// base 10 is 0 to 9
+// binary base 2 is 0 & 1
+
+console.log(0.1 + 0.2); //running joke here!
+
+console.log(Number('23'));
+console.log(+'23'); //shortcut conversion to number
+
+// parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e30', 10));
+
+console.log(Number.parseInt('  2.5rem  '));
+console.log(Number.parseFloat('  2.5rem'));
+// console.log(parseFloat('54.01')); don't do this - lacks a clear namespace
+
+console.log('--- isNaN ---'); // only checking for NaN, specifically
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('123'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN('23/0'));
+
+console.log('--- isFinite ---'); // prefer this one, or Number.isInteger when checking for numbers
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('123'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite('23/0'));
+
+console.log('--- isInteger ---');
+console.log(Number.isInteger(20));
+console.log(Number.isInteger('123'));
+console.log(Number.isInteger(+'20x'));
+console.log(Number.isInteger('23/0'));
