c9cec4585c9305c14600a1f6620bfde2376a8908
pragmaticpat <sully317@gmail.com>
2021-01-02 12:38:47 -0500

    Ive tried the main lifecycle events - pretty short lecture .. looking forward to next!

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -464,4 +464,20 @@ slider();
 //   if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
 
-// 192
+// 192 - was done inline
+
+// 197
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTMl parsed and DOM tree build!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page full loaded!', e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = ''; //for historical reasons
+});
