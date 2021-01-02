02ec2e8a7597070eee2eba82c9c29381af52b3dc
pragmaticpat <sully317@gmail.com>
2021-01-02 13:40:50 -0500

    getting into prototypal inheritance. i really like the degree of detail he is using to explain all of this!

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -15,7 +15,7 @@ console.log(jonas);
 
 // 1. a new empty object is created
 // 2. the function is called, this = {}
-// 3. {} is linked to a prototype
+// 3. {} is linked to a prototype => Creates the "proto" property
 // 4. function automatically returns {}
 
 const matilda = new Person('Matilda', 2017);
@@ -25,3 +25,25 @@ console.log(matilda, jack);
 const jay = { name: 'jay' };
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  // this is how to implement very basic prototypal inheritance in JavaScript
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person)); //NOT!
+
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(jonas.species, matilda.species);
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species')); //NOPE! because it's in the Person prototype
