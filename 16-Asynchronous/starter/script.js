1682b129f50558013a0183f8ae9a878fca52228a
pragmaticpat <sully317@gmail.com>
2021-01-23 14:08:19 -0500

    exploring implications of callback queue vs microtask queue, and priority between them

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,30 +1,30 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+//   // countriesContainer.style.opacity = 1;
+// };
 
-function renderCountry(data, className = '') {
-  const html = `<article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} million</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>`;
+// function renderCountry(data, className = '') {
+//   const html = `<article class="country ${className}">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)} million</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>`;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
-}
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+// countriesContainer.style.opacity = 1;
+// }
 ///////////////////////////////////////
 
 // function getCountryData(country) {
@@ -111,45 +111,45 @@ function renderCountry(data, className = '') {
 //       console.log(data);
 //       renderCountry(data[0]);
 //     });
+// // };
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country "${country}" not found! (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+
+//       // const neighbour = 'asdfas';
+//       const neighbour = data[0].borders[0];
+
+//       if (!neighbour) return;
+//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     })
+//     .then(nResponse => {
+//       if (nResponse) {
+//         console.log(nResponse);
+//         return nResponse.json();
+//       }
+//     })
+//     .then(data => {
+//       if (data) renderCountry(data, 'neighbour');
+//     })
+//     .catch(err => {
+//       console.error(`${err} 🔥🔥🔥`);
+//       renderError(`Something went wrong! 🔥🔥 ${err}. Try again.`);
+//     })
+//     .finally(() => {
+//       // good for spinners
+//       countriesContainer.style.opacity = 1;
+//     });
 // };
-
-const getCountryData = function (country) {
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(response => {
-      console.log(response);
-
-      if (!response.ok)
-        throw new Error(`Country "${country}" not found! (${response.status})`);
-
-      return response.json();
-    })
-    .then(data => {
-      renderCountry(data[0]);
-
-      // const neighbour = 'asdfas';
-      const neighbour = data[0].borders[0];
-
-      if (!neighbour) return;
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    })
-    .then(nResponse => {
-      if (nResponse) {
-        console.log(nResponse);
-        return nResponse.json();
-      }
-    })
-    .then(data => {
-      if (data) renderCountry(data, 'neighbour');
-    })
-    .catch(err => {
-      console.error(`${err} 🔥🔥🔥`);
-      renderError(`Something went wrong! 🔥🔥 ${err}. Try again.`);
-    })
-    .finally(() => {
-      // good for spinners
-      countriesContainer.style.opacity = 1;
-    });
-};
 
 // const getJSON = function (url, errorMessage = `Something went wrong`) {
 //   return fetch(url).then(response => {
@@ -220,22 +220,35 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(response => {
-      if (response.status === 403)
-        throw new Error(`💨 Exceeded API throttle ${response.status}`);
-      return response.json();
-    })
-    .then(json => {
-      console.log(`You are in ${json.city}, ${json.country}`);
-      getCountryData(json.country);
-    })
-    .catch(err => console.error(`😭 Unable to get country data! (${err})`));
-};
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(response => {
+//       if (response.status === 403)
+//         throw new Error(`💨 Exceeded API throttle ${response.status}`);
+//       return response.json();
+//     })
+//     .then(json => {
+//       console.log(`You are in ${json.city}, ${json.country}`);
+//       getCountryData(json.country);
+//     })
+//     .catch(err => console.error(`😭 Unable to get country data! (${err})`));
+// };
 
-btn.addEventListener('click', function () {
-  whereAmI(52.508, 13.381);
-  whereAmI(19.037, 72.873);
-  whereAmI(-33.933, 18.474);
-});
+// btn.addEventListener('click', function () {
+//   whereAmI(52.508, 13.381);
+//   whereAmI(19.037, 72.873);
+//   whereAmI(-33.933, 18.474);
+// });
+
+console.log('Test start'); //1
+setTimeout(() => {
+  console.log('zero second timer'); //5
+}, 0);
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res)); //3
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+}); //4
+
+console.log('test end'); //2
