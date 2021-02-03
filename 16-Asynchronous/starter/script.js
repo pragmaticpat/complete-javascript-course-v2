a53e616917afc5ab90751541b1a76fc270bbb013
pragmaticpat <sully317@gmail.com>
2021-02-02 19:29:25 -0500

    revisiting IIFEs, and favoring async await over then then then chains

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,7 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 function renderCountry(data, className = '') {
@@ -384,22 +384,54 @@ const getPosition = function () {
 };
 
 const whereAmI = async function () {
-  // Gelocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+  try {
+    // Gelocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const dataGeo = await resGeo.json();
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
 
-  // Country data
-  // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res))
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
-  ); // async await is just syntactic sugar over the .then approach (just above as reference)
-  const data = await res.json();
-  renderCountry(data[0]);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+    const dataGeo = await resGeo.json();
+
+    // Country data
+    // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res))
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.countryasdf}`
+    ); // async await is just syntactic sugar over the .then approach (just above as reference)
+    if (!res.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+  } catch (err) {
+    console.error(`${err} 💩`);
+    renderError(`🔥 ${err.message}`);
+
+    // reject promise returned from async function
+    throw err;
+  }
 };
 
-whereAmI('russia');
-console.log('FIRST');
+console.log('1: Will get location');
+
+// const city = whereAmI();
+// console.log(city);
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log('3: Finished'));
+// console.log('3: Finished getting location');
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: 💩 an error occurred! - ${err.message} 💩`);
+  } finally {
+    console.log('3: Finished getting location');
+  }
+})();
