089d17fdbacf1a15c522b6886ca70e76242583a1
pragmaticpat <sully317@gmail.com>
2021-02-02 19:47:51 -0500

    promises in parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -151,13 +151,13 @@ function renderCountry(data, className = '') {
 //     });
 // };
 
-// const getJSON = function (url, errorMessage = `Something went wrong`) {
-//   return fetch(url).then(response => {
-//     if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
+const getJSON = function (url, errorMessage = `Something went wrong`) {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
 
-//     return response.json();
-//   });
-// };
+    return response.json();
+  });
+};
 
 // const getCountryData = function (country) {
 //   // country 1
@@ -415,23 +415,49 @@ const whereAmI = async function () {
   }
 };
 
-console.log('1: Will get location');
+// console.log('1: Will get location');
 
-// const city = whereAmI();
-// console.log(city);
-// whereAmI()
-//   .then(city => console.log(`2: ${city}`))
-//   .catch(err => console.error(`2: ${err.message} 💥`))
-//   .finally(() => console.log('3: Finished'));
-// console.log('3: Finished getting location');
+// // const city = whereAmI();
+// // console.log(city);
+// // whereAmI()
+// //   .then(city => console.log(`2: ${city}`))
+// //   .catch(err => console.error(`2: ${err.message} 💥`))
+// //   .finally(() => console.log('3: Finished'));
+// // console.log('3: Finished getting location');
 
-(async function () {
+// (async function () {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (err) {
+//     console.error(`2: 💩 an error occurred! - ${err.message} 💩`);
+//   } finally {
+//     console.log('3: Finished getting location');
+//   }
+// })();
+
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const city = await whereAmI();
-    console.log(`2: ${city}`);
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
-    console.error(`2: 💩 an error occurred! - ${err.message} 💩`);
-  } finally {
-    console.log('3: Finished getting location');
+    console.log(`💩 Error occurred: ${err.message}`);
   }
-})();
+};
+
+get3Countries('ireland', 'poland', 'hungary');
